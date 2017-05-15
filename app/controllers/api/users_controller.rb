645d2665c9a9fc2add43b81454ac009cb1d7940c
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-15 13:37:02 -0700

    Add user routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -16,6 +16,15 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(update_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:show, :create, :update]
+    resource :session, only: [:create, :destroy, :show]
+
+    # resources :posts
+    # resources :likes, only: [:create, :destroy]
+    # resources :comments, only: [:create, :destroy]
+    # # comments nested under posts
+    #
+    # resources :follows, only: [:create, :destroy]
+    # get "follows/delete", to: "follows#delete"
+    #
+    # resources :search_results, only: [:index]
+  end
 end
