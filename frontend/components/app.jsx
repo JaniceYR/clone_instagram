ea2310aa52a527f3c32695117a209a9bc3d7ce96
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-15 14:51:09 -0700

    Add frontend authentication

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,8 +8,8 @@ import SessionFormContainer from './session/session_form_container';
 const App = () => (
   <div>
     <header>
-      <Link to="/" className="header-link">
-        <h1>Bench BnB</h1>
+      <Link to="/">
+        <h1>CloneInstagram</h1>
       </Link>
     </header>
     <Switch>
