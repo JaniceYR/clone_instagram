cc67346b0aa2f0b35d80306972caa1b4397a563c
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-15 14:03:40 -0700

    Add frontend skeleton

---
+++ frontend/components/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+import SessionFormContainer from './session_form/session_form_container';
+
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/" className="header-link">
+        <h1>Bench BnB</h1>
+      </Link>
+    </header>
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
