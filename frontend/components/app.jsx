1d906e6e9c41e75c541504bcbbd39c3ff980c41f
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-17 10:11:32 -0700

    Add skeleton

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,15 +3,17 @@ import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 
 import SessionFormContainer from './session/session_form_container';
-
+import FeedContainer from './feed/feed_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div>
         <h1>App main page</h1>
-        <Route path="/login" component={SessionFormContainer} />
-        <Route path="/signup" component={SessionFormContainer} />
-
+        <Switch>
+          <Route path="/login" component={SessionFormContainer} />
+          <Route path="/signup" component={SessionFormContainer} />
+          <Route path="/feed" component={FeedContainer} />
+        </Switch>
     </div>
 );
 
---
+++ frontend/components/feed/feed_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+
+import UserProfile from './user_profile';
+
+const mapStateToProps = (state, ownProps) => ({
+});
+
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserProfile);
