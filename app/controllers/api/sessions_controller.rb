1cf2ccd0e0ad54d00c652e0e33840d737ac6a47c
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-17 09:22:05 -0700

    Fix render error part on session

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,7 +8,7 @@ class Api::SessionsController < ApplicationController
       login(@user)
       render "api/users/show"
     else
-      render json: { login: ["Invalid username/password"] }, status: 401
+      render json: ["Invalid username/password"], status: 401
     end
   end
 
