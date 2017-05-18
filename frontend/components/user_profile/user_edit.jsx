0f96b58e81148c37ef836e605ff75134cf6d5a22
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-18 09:29:18 -0700

    Start Day3

--- frontend/components/user_profile/user_edit.jsx
+++ frontend/components/user_profile/user_edit.jsx
@@ -40,38 +40,40 @@ class UserEdit extends React.Component {
       <div>
         <Navigation />
         <h1>user profile edit page</h1>
-        <div>
-          <div>
-            <h1>Edit Profile</h1>
-          </div>
-          <div>
-            <div>
-                <div>
-                  here is for user profile pic{this.props.user.profile_pic_url}
-                </div>
-                <div>
-                  <h1>{this.props.user.username}</h1>
-                </div>
+        <div className="edit-page">
+          <div className="edit-frame-merge">
+            <div className="edit-sub-frame-left">
+              <h1>Edit Profile</h1>
             </div>
-            <div>
-              <form onSubmit={this.handleSubmit}>
-                <div>
-                  <h1>Name</h1>
-                  <input type="text" onChange={this.update('name')} value={this.state.name} placeholder="Full Name"></input>
-                </div>
-                <div>
-                  <h1>Username</h1>
-                  <input type="text" onChange={this.update('username')} value={this.state.username} placeholder="username"></input>
-                </div>
-                <div>
-                  <h1>Bio</h1>
-                  <input type="text" onChange={this.update('bio')} value={this.state.bio} placeholder="Bio"></input>
-                </div>
-                <div>
-                  <h1> </h1>
-                  <input type="submit" value="Submit"></input>
-                </div>
-              </form>
+            <div className="edit-sub-frame-right">
+              <div className="user-pic-frame">
+                  <div>
+                    here is for user profile pic{this.props.user.profile_pic_url}
+                  </div>
+                  <div>
+                    <h1>{this.props.user.username}</h1>
+                  </div>
+              </div>
+              <div>
+                <form onSubmit={this.handleSubmit}>
+                  <div>
+                    <h1>Name</h1>
+                    <input type="text" onChange={this.update('name')} value={this.state.name} placeholder="Full Name"></input>
+                  </div>
+                  <div>
+                    <h1>Username</h1>
+                    <input type="text" onChange={this.update('username')} value={this.state.username} placeholder="username"></input>
+                  </div>
+                  <div>
+                    <h1>Bio</h1>
+                    <input type="text" onChange={this.update('bio')} value={this.state.bio} placeholder="Bio"></input>
+                  </div>
+                  <div>
+                    <h1> </h1>
+                    <input type="submit" value="Submit"></input>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
