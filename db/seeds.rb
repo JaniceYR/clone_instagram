9342418ff7e19c40ac7be762c9cf1ce645ec263a
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-22 14:32:28 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Post.destroy_all
+Follow.destroy_all
+Like.destroy_all
+Comment.destroy_all
 
 user1 = User.create(username: "demoID", password: "password",
                     name: "Janice Lee", bio: "Hi there! This id is for demo. :)", profile_pic_url: "https://res.cloudinary.com/cloneinstagram/image/upload/v1495406902/1662763_1155477257805616_1310834924_a_xmqzhi.jpg")
@@ -38,3 +41,41 @@ post9 = Post.create(user_id: user2.id, photo_url: "https://res.cloudinary.com/cl
             description: "소샬리토의 새벽", location: "Sausalito")
 post10 = Post.create(user_id: user2.id, photo_url: "https://res.cloudinary.com/cloneinstagram/image/upload/v1495134767/17438109_789314367891321_6968004522685759488_n_zchlgp.jpg",
             description: "미국에 놀러온 언니", location: "Phoenix")
+
+follow1 = Follow.create(follower_id: user1.id, following_id: user2.id)
+follow2 = Follow.create(follower_id: user1.id, following_id: user3.id)
+follow3 = Follow.create(follower_id: user2.id, following_id: user1.id)
+follow4 = Follow.create(follower_id: user3.id, following_id: user1.id)
+follow5 = Follow.create(follower_id: user4.id, following_id: user1.id)
+
+likes1 = Like.create(user_id: user1.id, post_id: post1.id)
+likes2 = Like.create(user_id: user1.id, post_id: post3.id)
+likes3 = Like.create(user_id: user2.id, post_id: post1.id)
+likes4 = Like.create(user_id: user3.id, post_id: post1.id)
+likes5 = Like.create(user_id: user2.id, post_id: post2.id)
+
+comment1 = Comment.create(user_id: user1.id, post_id: post1.id,
+                body: "1st comment from user1")
+comment2 = Comment.create(user_id: user2.id, post_id: post1.id,
+                body: "nice pic!")
+comment3 = Comment.create(user_id: user3.id, post_id: post1.id,
+                body: "Copyright (c) 2017 Copyright Holder All Rights Reserved.")
+comment4 = Comment.create(user_id: user4.id, post_id: post1.id,
+                body: "Coffee made from house-roasted beans served along with baked goods in a bi-level, industrial space.
+Address: 270 7th St, San Francisco, CA 94103
+Hours: Open today · 7AM–7PM
+Phone: (415) 861-1313
+Menu: places.singleplatform.com")
+comment5 = Comment.create(user_id: user1.id, post_id: post1.id,
+                body: "Thanks for details!")
+
+comment6 = Comment.create(user_id: user2.id, post_id: post2.id,
+                body: "where is it?")
+comment7 = Comment.create(user_id: user3.id, post_id: post2.id,
+                body: "awesome!")
+comment8 = Comment.create(user_id: user4.id, post_id: post3.id,
+                body: "멋지당")
+comment9 = Comment.create(user_id: user1.id, post_id: post3.id,
+                body: "나중에 같이 가자")
+comment10 = Comment.create(user_id: user2.id, post_id: post3.id,
+                body: "하하하하하하하하하하하하하하")
