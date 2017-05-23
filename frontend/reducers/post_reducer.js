821c4b37c60ccce0f35a952ab19d15ec97742e8a
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-22 17:35:01 -0700

    Add receive and remove on post reducer

--- frontend/reducers/post_reducer.js
+++ frontend/reducers/post_reducer.js
@@ -1,5 +1,7 @@
-import { RECEIVE_POST, REMOVE_POST, RECEIVE_POSTS } from '../actions/post_actions';
 import merge  from 'lodash/merge';
+
+import { RECEIVE_POST, REMOVE_POST, RECEIVE_POSTS } from '../actions/post_actions';
+import { RECEIVE_LIKE, REMOVE_LIKE } from '../actions/like_actions';
 
 const PostsReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -12,6 +14,10 @@ const PostsReducer = (state = {}, action) => {
       let newState = merge({}, state);
       delete newState[action.post.id];
       return newState;
+    case RECEIVE_LIKE:
+      return merge({}, state, { liked: true });
+    case REMOVE_LIKE:
+      return merge({}, state, { liked: false });
     default:
       return state;
   }
