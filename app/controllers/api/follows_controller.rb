67dd25f3bf2c819f2ed7ca00cb8e9ff3fd98908c
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-25 17:29:24 -0700

    Fix to be able to follow the other user on suggestion page

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -2,7 +2,7 @@ class Api::FollowsController < ApplicationController
   def create
     @follow = Follow.new(follow_params)
     if @follow.save
-      render "api/follows/show"
+      render json: @follow
     else
       render json: @follow.errors.full_messages, status: 422
     end
--- frontend/reducers/suggestion_reducer.js
+++ frontend/reducers/suggestion_reducer.js
@@ -1,12 +1,21 @@
 import merge  from 'lodash/merge';
 
 import { RECEIVE_USERS } from '../actions/user_actions';
+import { RECEIVE_FOLLOW, REMOVE_FOLLOW } from '../actions/follow_actions';
 
 const SuggestionReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_USERS:
       return action.users;
+    case RECEIVE_FOLLOW:
+      let addFollow = merge({}, state);
+      addFollow[action.follow.following_id].followed = true;
+      return addFollow;
+    case REMOVE_FOLLOW:
+      let removeFollow = merge({}, state);
+      removeFollow[action.follow.following_id].followed = false;
+      return removeFollow;
     default:
       return state;
   }
